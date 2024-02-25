dd7bf4bdba2bdbc8aaa93d7ab5556a770c1ca28d
Sharyberry <145032663+Sharyberry@users.noreply.github.com>
2024-02-25 21:13:33 +0200

    Update open source links

--- src/App.js
+++ src/App.js
@@ -8,6 +8,18 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
         <Weather />
+        <small>
+          <a
+            href="https://github.com/Sharyberry/React-app-weather"
+            target="_blank"
+          >
+            Open-source code
+          </a>
+          by
+          <a href="https://www.instagram.com/sharyberry_m/" target="_blank">
+            Sharon Mahlakeng
+          </a>
+        </small>
       </header>
     </div>
   );
